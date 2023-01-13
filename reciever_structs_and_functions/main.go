1de934168cb8e57c7c763f98bb6530d17a227de1
TheViralClovers <theviralclovers@gmail.com>
2023-01-13 17:07:09 +0530

    update receiver functions
    Updated details about receiver functions

--- reciever_structs_and_functions/main.go
+++ reciever_structs_and_functions/main.go
@@ -1,1 +1,27 @@
 package reciever_structs_and_functions
+
+import "log"
+
+type myStruct struct {
+	FirstName string
+}
+
+func (m *myStruct) printFirstName() string { //this is a receiver function, which receives a structure myStruct as m
+	return m.FirstName
+}
+
+func main() {
+	var myVar myStruct //variable myVar is set to type myStruct
+	myVar.FirstName = "John"
+
+	myVar2 := myStruct{ //another way to declare structs (shorthand)
+		FirstName: "Mary",
+	}
+
+	log.Println(myVar.FirstName)  //accessing a variable inside a structure, by calling it directly
+	log.Println(myVar2.FirstName) //same
+
+	//Now let us access it using the "receiver function created above
+	log.Println(myVar.printFirstName()) //This return the FirstName and also gets printed
+	log.Println(myVar.printFirstName())
+}
